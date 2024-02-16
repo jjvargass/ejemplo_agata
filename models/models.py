f1a0765adb98e16004c1341a7404d4d25bbbb22f
jjvargass <jotavargas89@gmail.com>
2024-02-16 14:53:02 -0500

    creación de menús para reportes

--- models/models.py
+++ models/models.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class ejemplo_agata(models.Model):
-#     _name = 'ejemplo_agata.ejemplo_agata'
-#     _description = 'ejemplo_agata.ejemplo_agata'
+class ejemplo_agata(models.Model):
+    _name = 'ejemplo_agata.ejemplo_agata'
+    _description = 'ejemplo_agata.ejemplo_agata'
 
-#     name = fields.Char()
+    name = fields.Char()
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
